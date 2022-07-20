729a9414d88d4a3a7177d30cd14b95c7dd244368
nh325 <104600815+nh325@users.noreply.github.com>
2022-07-20 11:21:46 -0400

    Update and rename stage_pmt_system.py to stage_pmt_daq.py

--- stage_pmt_daq.py
+++ stage_pmt_daq.py
@@ -26,7 +26,7 @@ def system(ms2k, pixelnum_x, pixelnum_y, pixelsize, samp_collect, samp_rate):
     
     with nidaqmx.Task() as task:
         task.ai_channels.add_ai_voltage_chan("Dev2/ai0")
-        task.timing.cfg_samp_clk_timing(samp_rate, sample_mode = AcquisitionType.FINITE, samps_per_chan = samp_collect * 10000)
+        task.timing.cfg_samp_clk_timing(samp_rate, sample_mode = AcquisitionType.FINITE, samps_per_chan = samp_collect * 100000)
         task.start()
         
         print('Calibrating...')
